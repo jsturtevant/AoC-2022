9d672e27544d8cd09dbcc0c1fbb0c806811c35d8
James Sturtevant <jstur@microsoft.com>
2022-12-22 21:39:15 +0000

    re-use parsing
    Signed-off-by: James Sturtevant <jstur@microsoft.com>

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -1,9 +1,8 @@
-use std::fs::File;
-use std::io::{self, BufRead};
-use std::path::Path;
+
+use file_import;
 
 fn main() {
-    if let Ok(lines) = read_lines("./input.txt") {
+    if let Ok(lines) = file_import::getlines("./input.txt") {
         
         let mut calorie_totals = Vec::new();
 
@@ -32,11 +31,4 @@ fn main() {
 
         println!("total for the top three {:?}",  top_three_total);
     }
-}
-
-// https://doc.rust-lang.org/stable/rust-by-example/std_misc/arg/matching.html
-fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
-where P: AsRef<Path>, {
-    let file = File::open(filename)?;
-    Ok(io::BufReader::new(file).lines())
 }
--- day2/src/main.rs
+++ day2/src/main.rs
@@ -1,10 +1,9 @@
 use std::collections::HashMap;
-use std::fs::File;
-use std::io::{self, BufRead};
-use std::path::Path;
+use file_import;
 
 fn main() {
-    if let Ok(lines) = read_lines("./input.txt") {
+    if let Ok(lines) = file_import::getlines("./input.txt") {
+        
         let mut game = RpsGame::new();
 
         game.game_rules.insert(
@@ -130,11 +129,3 @@ impl RpsGame {
     }
 }
 
-// https://doc.rust-lang.org/stable/rust-by-example/std_misc/arg/matching.html
-fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
-where
-    P: AsRef<Path>,
-{
-    let file = File::open(filename)?;
-    Ok(io::BufReader::new(file).lines())
-}
